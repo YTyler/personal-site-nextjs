78c623afc3bfe6f6a7697a9a1ca691afd426f694
Tyler C. Yates <tyates907@gmail.com>
2021-03-31 15:26:25 -0700

    style navbar & add buttons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,10 @@ import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <div>
+    <div class={styles.bar}>
+      <a href="/" class={styles.bar__button}>Home</a>
+      <a href="Portfolio" class={styles.bar__button}>Portfolio</a>
+      <a href="Contact" class={styles.bar__button}>Contact Info</a>
     </div>
   );
 }
