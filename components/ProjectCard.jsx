bd92f4b51ccfc3b9e797333c841e3d01f295c903
Tyler C. Yates <tyates907@gmail.com>
2021-04-02 15:45:02 -0700

    change classnames

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,8 +5,8 @@ import styles from '../styles/ProjectCard.module.css'
 export default function ProjectCard({ url, alttext }) {
   return (
     <div class={styles.card}>
-      <div class={styles.card__image}>
-        <Image class={styles.card__image} src={url} alt={alttext} layout='responsive' width={260} height={200} />
+      <div class={styles.image_container}>
+        <Image class={styles.card__image} src={url} alt={alttext} layout='responsive' width={130} height={100} />
       </div>
       <h1>Project Name</h1>
       <p>Project Description dolor sit amet consectetur adipisicing elit. Impedit pariatur necessitatibus vero porro sint nobis ab aliquid amet doloribus aperiam animi beatae quaerat unde eius vel aspernatur, dolorum libero dignissimos.</p>
