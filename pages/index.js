3483c7942eb7b670c7186ca859f73919e4745f5f
Tyler C. Yates <tyates907@gmail.com>
2021-04-02 10:07:59 -0700

    remove  color

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
           <div class={styles.hero}>
             <div class='wrapper'>
               <div class={styles.aboutMe}>
-                <h1>About me</h1>
+                <h1>About Me</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh praesent tristique magna sit amet. Sed elementum tempus egestas sed sed risus pretium quam vulputate. Non consectetur a erat nam at lectus urna duis. Et egestas quis ipsum suspendisse ultrices. Ullamcorper morbi tincidunt ornare massa. Aliquet risus feugiat in ante metus dictum at tempor. Tincidunt lobortis feugiat vivamus at augue eget arcu.</p>
               </div>
             </div>
