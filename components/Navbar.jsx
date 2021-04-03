0685fe6376b594f1bcf24d2a51c0acf8921d9789
Tyler C. Yates <tyates907@gmail.com>
2021-04-02 17:44:32 -0700

    replace 'a' tag with nextjs Link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <div class={styles.bar}>
-      <a href="/" class={styles.bar__button}>Home</a>
-      <a href="Portfolio" class={styles.bar__button}>Portfolio</a>
-      <a href="Contact" class={styles.bar__button}>Contact</a>
-      <a href="Test" class={styles.bar__button}>Test</a>
+      <Link href="/" ><a class={styles.bar__button}> Home</a></Link>
+      <Link href="/Portfolio"><a class={styles.bar__button}>Portfolio</a></Link>
+      <Link href="/Contact" ><a class={styles.bar__button}>Contact</a></Link>
+      <Link href="/Test" ><a class={styles.bar__button}>Test</a></Link>
     </div>
   );
 }
